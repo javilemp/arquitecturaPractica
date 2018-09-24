212717c386bd435da228066c64af4c72871a8e64
Fernando Artola <fernando.artola@hotmail.com>
2018-09-24 09:21:42 -0300

    Showing an Individual Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,12 @@ class PostsController < ApplicationController
 
 
   def show
+	@post = Post.find(params[:id])
+
+	respond_to do |format|
+	format.html  # show.html.erb
+	format.json  { render :json => @post }
+  end
+
   end
 end
